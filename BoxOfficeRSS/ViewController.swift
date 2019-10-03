bf4cec014620a533ca1371b7e98036e0fb2e05da
izuru nomura <izuru.nomura@gmail.com>
2019-10-03 22:56:00 +0900

    Initial Commit

---
+++ BoxOfficeRSS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoxOfficeRSS
+//
+//  Created by izuru nomura on 2019/10/03.
+//  Copyright © 2019 izuru nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
